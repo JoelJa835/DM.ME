6999fbb8c2aa5cd027cb1db4629385c0bc28b695
joel jani <joeljani71@gmail.com>
2021-02-11 23:30:05 +0200

    TI KANW

---
+++ Scraper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from bs4 import BeautifulSoup
+
+try:
+    html = urlopen("https://www.skroutz.gr/s/25999039/Gigabyte-RTX-3060-Ti-Gaming-OC-8GB-GV-N306TGAMING-OC-8GD.html")
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print("The server could not be found!")
+else:
+    bsobj = BeautifulSoup(html, 'lxml')
+    gpu = bsobj.findAll("a", {"class":"sku-image"})
+    print(gpu)
+
