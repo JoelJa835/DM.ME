903aeaca09c76e7eed45bfcf6fb919055de822d6
joel jani <joeljani71@gmail.com>
2021-02-21 18:56:44 +0200

    Changed modules and got acces to gpu titles and prices

--- Scraper.py
+++ Scraper.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.error import URLError
+import requests
 from bs4 import BeautifulSoup
 
+
+
 try:
-    html = urlopen("https://www.skroutz.gr/s/25999039/Gigabyte-RTX-3060-Ti-Gaming-OC-8GB-GV-N306TGAMING-OC-8GD.html")
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print("The server could not be found!")
+    html_text = requests.get('https://www.skroutz.gr/c/55/kartes-grafikwn-vga.html?o=gpu').text
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+except requests.exceptions.ConnectionError as errc:
+    print ("Error Connecting:",errc)
+except requests.exceptions.Timeout as errt:
+    print ("Timeout Error:",errt)
+except requests.exceptions.RequestException as err:
+    print ("OOps: Something Else",err)
 else:
-    bsobj = BeautifulSoup(html, 'lxml')
-    gpu = bsobj.findAll("a", {"class":"sku-image"})
-    print(gpu)
+    soup = BeautifulSoup(html_text, 'lxml')
+    gpus = soup.findAll('li', class_ = 'cf card')
+    for gpu in gpus:
+        gpu_content = gpu.find('div', class_ = 'card-content')
+        gpu_title = gpu_content.find('a', class_ = 'js-sku-link').get('title')
+        gpu_price = gpu_content.find('a',class_ = 'js-sku-link sku-link').text.replace('από','')
+        print(f'{gpu_title} costs {gpu_price}')
 
