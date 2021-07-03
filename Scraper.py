7f0948f1257cd95c7d14e4e04869240fe0695e9d
Joel <kapios8579@gmail.com>
2021-07-03 13:43:23 +0300

    made code cleaner and added comments

--- Scraper.py
+++ Scraper.py
@@ -5,6 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
+#Inserting scraped data to database
 def InsertToDatabase(gpuList):
     try:
         sqliteConnection = sqlite3.connect('SQLite_Python.db')
@@ -26,39 +27,49 @@ def InsertToDatabase(gpuList):
             sqliteConnection.close()
             print("The SQLite connection is closed")
 
-try:
-    html_text = requests.get('https://www.skroutz.gr/c/55/kartes-grafikwn-vga.html?o=gpu').text
-except requests.exceptions.HTTPError as errh:
-    print ("Http Error:",errh)
-except requests.exceptions.ConnectionError as errc:
-    print ("Error Connecting:",errc)
-except requests.exceptions.Timeout as errt:
-    print ("Timeout Error:",errt)
-except requests.exceptions.RequestException as err:
-    print ("OOps: Something Else",err)
-else:
-    list = []
+
+
+def ScrapingGpus():
+    #Performing get request to below link to get html code
+    try:
+        html_text = requests.get('https://www.skroutz.gr/c/55/kartes-grafikwn-vga.html?o=gpu').text
+    except requests.exceptions.HTTPError as errh:
+        print ("Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error:",errt)
+    except requests.exceptions.RequestException as err:
+        print ("OOps: Something Else",err)
+    else:
+        list = []
+        #Connecting to database
+        sqliteConnection = sqlite3.connect('SQLite_Python.db')
+        sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS Gpus (
+                                    Title TEXT NOT NULL, Price TEXT NOT NULL)'''
+
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+        cursor.execute(sqlite_create_table_query)
+        sqliteConnection.commit()
+        print("SQLite table created")
+        #Parsing html code
+        soup = BeautifulSoup(html_text, 'lxml')
+        gpus = soup.findAll('li', class_ = 'cf card')
+        #Find all gpus and scraping info
+        for gpu in gpus:
+            gpu_content = gpu.find('div', class_ = 'card-content')
+            gpu_title = gpu_content.find('a', class_ = 'js-sku-link').get('title')
+            gpu_price = gpu_content.find('a',class_ = 'js-sku-link sku-link').text.replace('€','').replace('από','')
+            GPU=(gpu_title,gpu_price)
+            list.append(GPU)
+        InsertToDatabase(list)
+
+#Retrieving data from database and printing in console
+def RetrieveFromDb():
     sqliteConnection = sqlite3.connect('SQLite_Python.db')
-    sqlite_create_table_query = '''CREATE TABLE Gpus (
-                                Title TEXT NOT NULL, Price TEXT NOT NULL)'''
-
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
-    cursor.execute(sqlite_create_table_query)
-    sqliteConnection.commit()
-    print("SQLite table created")
-
-    soup = BeautifulSoup(html_text, 'lxml')
-    gpus = soup.findAll('li', class_ = 'cf card')
-    for gpu in gpus:
-        gpu_content = gpu.find('div', class_ = 'card-content')
-        gpu_title = gpu_content.find('a', class_ = 'js-sku-link').get('title')
-        gpu_price = gpu_content.find('a',class_ = 'js-sku-link sku-link').text.replace('€','').replace('από','')
-        GPU=(gpu_title,gpu_price)
-        list.append(GPU)
-    InsertToDatabase(list)
-
     sqlite_select_query = """SELECT * from Gpus"""
+    cursor = sqliteConnection.cursor()
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
     print("Total rows are:  ", len(records))
@@ -68,3 +79,11 @@ else:
         print("Price: ", row[1])
     cursor.close()
 
+def main():
+    ScrapingGpus()
+    RetrieveFromDb()
+        
+
+if __name__ == "__main__":
+
+    main()
